74250fc1335e067cc480933e493ffdadc3995233
JungWook <kjo6391@gmail.com>
2023-06-02 12:32:03 +0900

    html

--- Nexus/src/main/java/com/ln/intranet/attendance/controller/AttendanceController.java
+++ Nexus/src/main/java/com/ln/intranet/attendance/controller/AttendanceController.java
@@ -1,13 +1,24 @@
 package com.ln.intranet.attendance.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.ln.intranet.attendance.model.service.AttendanceService;
+import com.ln.intranet.attendance.model.vo.Attendance;
 
 @Controller
 public class AttendanceController {
 
 	@Autowired
 	AttendanceService service;
+	
+	public void selectAttendanceList() {
+		
+		List<Attendance> attendanceList = new ArrayList<Attendance>();
+		
+		attendanceList = service.selectAttendanceList();
+	}
 }
--- Nexus/src/main/java/com/ln/intranet/attendance/model/service/AttendanceServiceImp.java
+++ Nexus/src/main/java/com/ln/intranet/attendance/model/service/AttendanceServiceImp.java
@@ -1,13 +1,22 @@
 package com.ln.intranet.attendance.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ln.intranet.attendance.model.dao.AttendanceDAO;
+import com.ln.intranet.attendance.model.vo.Attendance;
 
 @Service
 public class AttendanceServiceImp implements AttendanceService {
 
 	@Autowired
 	AttendanceDAO dao;
+	
+	@Override
+	public List<Attendance> selectAttendanceList() {
+		
+		return dao.selectAttendanceList();
+	}
 }
