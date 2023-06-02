30ce4e440c50bd64ada43d3c59a352da15ea136a
JungWook87 <kjo6391@gmail.com>
2023-06-02 12:32:13 +0900

    notice & mapper

---
+++ Nexus/src/main/java/com/ln/intranet/notice/model/service/NoticeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ln.intranet.notice.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ln.intranet.notice.model.dao.NoticeDao;
+import com.ln.intranet.notice.model.vo.Notice;
+
+public class NoticeServiceImpl implements NoticeService{
+	
+	@Autowired
+	private NoticeDao dao;
+
+	@Override
+	public List<Notice> selectNoticeList(int deptCode) {
+		return dao.selectNoticeList(deptCode);
+	}
+
+}
