4bf9eb1560b555d28a1a2132b67013ede9592996
JungWook87 <kjo6391@gmail.com>
2023-06-02 13:58:01 +0900

    123

---
+++ Nexus/src/main/java/com/ln/intranet/notice/model/service/NoticeService.java
@@ -0,0 +1,11 @@
+package com.ln.intranet.notice.model.service;
+
+import java.util.List;
+
+import com.ln.intranet.notice.model.vo.Notice;
+
+public interface NoticeService {
+
+	List<Notice> selectNoticeList(int deptCode);
+
+}
