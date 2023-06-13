ba6d42029b4dccd264dde2d89a689c554235442e
FORMEEEEE <mrryu555@gmail.com>
2023-06-13 09:01:08 +0900

    0613

--- Nexus/src/main/java/com/ln/intranet/chat/model/service/ChatServiceImpl.java
+++ Nexus/src/main/java/com/ln/intranet/chat/model/service/ChatServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ln.intranet.chat.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ln.intranet.chat.model.dao.ChatDAO;
 
 @Service
 public class ChatServiceImpl implements ChatService{
 
+	@Autowired
+	private ChatDAO dao;
 }
