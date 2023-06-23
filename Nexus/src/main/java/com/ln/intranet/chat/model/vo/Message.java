a92a4b90e7bf06ebec572caf5ab10e96510723ed
FORMEEEEE <mrryu555@gmail.com>
2023-06-23 09:10:01 +0900

    vo수정

--- Nexus/src/main/java/com/ln/intranet/chat/model/vo/Message.java
+++ Nexus/src/main/java/com/ln/intranet/chat/model/vo/Message.java
@@ -2,6 +2,8 @@ package com.ln.intranet.chat.model.vo;
 
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,10 +13,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Message {
 
+	@JsonProperty("mNo")
 	private int mNo;
 	private int cmNo;
 	private int memNo;
+	@JsonProperty("mContent")
 	private String mContent;
+	@JsonProperty("mDate")
 	private Date mDate;
 	private String memberNick;
 	
