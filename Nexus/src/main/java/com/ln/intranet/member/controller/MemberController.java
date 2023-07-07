eb5e3e5ed9bdf99e57565f0114d1a01176779108
LJAEH <wogurdlek33@gmail.com>
2023-07-07 12:20:07 +0900

    0707

--- Nexus/src/main/java/com/ln/intranet/member/controller/MemberController.java
+++ Nexus/src/main/java/com/ln/intranet/member/controller/MemberController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,7 +24,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.google.gson.Gson;
-import com.ln.intranet.dept.model.vo.BoardDetail;
 import com.ln.intranet.member.model.service.MemberService;
 import com.ln.intranet.member.model.vo.Member;
 import com.ln.intranet.notice.model.service.NoticeService;
